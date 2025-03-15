1af553955d4f94e5e98b51991ab889c62c876262
thornrampai <thornrampai.t@ku.th>
2025-03-15 17:04:46 +0700

    image

--- lib/page/user/search.dart
+++ lib/page/user/search.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project/page/user/employee.dart';
-import 'package:project/service.dart/firestore.dart';
+import 'package:project/service/firestore.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -63,83 +63,102 @@ class _SearchPageState extends State<SearchPage> {
                   itemCount: employees.length,
                   itemBuilder: (context, index) {
                     var doc = employees[index];
-                    String image = doc['photo'];
+                    String imagePath =
+                        doc['imageUrl']; // Path ของไฟล์ใน Storage
                     String name = doc['name'] ?? 'Unknown';
-                    String type = doc['position'] ?? '';
+                    String type = doc['serviceType'] ?? '';
+                   
+                    return FutureBuilder<String>(
+                      future: firestoreService.getImageEmpolyee(
+                        imagePath,
+                      ), // ดึง URL ของรูปจาก Storage
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return SizedBox.shrink(); // โหลด URL อยู่
+                        }
 
-                    return InkWell(
-                      onTap: () {
-                        // เมื่อคลิกที่รายการให้ไปที่หน้า EmployeePage
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder:
-                                (context) => EmployeePage(
-                                  name: name,
-                                  image: image,
-                                  type: type,
-                                ),
-                          ),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 25),
-                        child: Center(
-                          child: SizedBox(
-                            height: 300,
-                            width: 300,
-                            child: Card(
-                              elevation: 5,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
+                        String imageUrl = snapshot.data!;
+
+                        return InkWell(
+                          onTap: () {
+                            // ไปที่หน้า EmployeePage
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (context) => EmployeePage(
+                                      name: name,
+                                      image: imageUrl,
+                                      type: type,
+                                    ),
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.star, color: Colors.amber),
-                                          SizedBox(width: 4),
-                                          Text(
-                                            '5 star', // ปรับตามข้อมูลจริง
-                                            style: TextStyle(fontSize: 16),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 25),
+                            child: Center(
+                              child: SizedBox(
+                                height: 300,
+                                width: 300,
+                                child: Card(
+                                  elevation: 5,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Row(
+                                            children: [
+                                              Icon(
+                                                Icons.star,
+                                                color: Colors.amber,
+                                              ),
+                                              SizedBox(width: 4),
+                                              Text(
+                                                '5 star', // ปรับตามข้อมูลจริง
+                                                style: TextStyle(fontSize: 16),
+                                              ),
+                                            ],
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                        SizedBox(height: 5),
+                                        CircleAvatar(
+                                          backgroundColor: Colors.grey,
+                                          radius: 70,
+                                          backgroundImage: NetworkImage(
+                                            imageUrl,
+                                          ),
+                                        ),
+                                        SizedBox(height: 15),
+                                        Text(
+                                          name,
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        SizedBox(height: 15),
+                                        Text(
+                                          type,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    SizedBox(height: 5),
-                                    CircleAvatar(
-                                      backgroundColor: Colors.grey,
-                                      radius: 70,
-                                      backgroundImage: NetworkImage(image),
-                                    ),
-                                    SizedBox(height: 15),
-                                    Text(
-                                      name,
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(height: 15),
-                                    Text(
-                                      type,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                  ],
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
+                        );
+                      },
                     );
                   },
                 );
---
+++ lib/service/firestore.dart
@@ -0,0 +1,334 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:project/page/user/pay_Success.dart';
+import 'package:project/provider/authProvider.dart';
+import 'package:project/widget/warnphonno.dart';
+import 'package:provider/provider.dart';
+
+class FirestoreService {
+  // ข้อมูลพนักงาน
+  final CollectionReference employee = FirebaseFirestore.instance.collection(
+    'Employee',
+  );
+
+  Stream<QuerySnapshot> getWhereDeepCleanStream() {
+    final deepCleanStream =
+        employee.where('serviceType', isEqualTo: 'แม่บ้าน').snapshots();
+    return deepCleanStream;
+  }
+
+  Stream<QuerySnapshot> getWhereGardenStream() {
+    final deepCleanStream =
+        employee.where('serviceType', isEqualTo: 'คนสวน').snapshots();
+    return deepCleanStream;
+  }
+
+  Stream<QuerySnapshot> getWhereCareStream() {
+    final deepCleanStream =
+        employee.where('serviceType', isEqualTo: "ดูแลผู้สูงอายุ").snapshots();
+    return deepCleanStream;
+  }
+
+  Stream<QuerySnapshot> getWherePetStream() {
+    final deepCleanStream =
+        employee.where('serviceType', isEqualTo: 'ดูแลสัตว์').snapshots();
+    return deepCleanStream;
+  }
+
+  Stream<QuerySnapshot> getEmployee() {
+    final employeeSteam = employee.orderBy('name').snapshots();
+
+    return employeeSteam;
+  }
+
+  Stream<QuerySnapshot> searchEmployee(String query) {
+    if (query.isEmpty) {
+      return getEmployee(); // ถ้าไม่มีคำค้นหาให้ดึงข้อมูลทั้งหมด
+    }
+
+    return employee
+        .where('name', isGreaterThanOrEqualTo: query)
+        .where('name', isLessThanOrEqualTo: query + '\uf8ff') // ค้นหาแบบ prefix
+        .snapshots();
+  }
+
+  Stream<String> getServiceTypeStream(String employeeName) {
+    return employee
+        .where('name', isEqualTo: employeeName)
+        .limit(1) // จำกัดผลลัพธ์ให้ได้แค่ 1 เอกสาร
+        .snapshots() // รับข้อมูลแบบสตรีม
+        .map((snapshot) {
+          if (snapshot.docs.isNotEmpty) {
+            return snapshot.docs.first['serviceType'] ?? 'ไม่พบข้อมูล';
+          }
+          return 'ไม่พบข้อมูล';
+        });
+  }
+
+//   Future<String> getImageEmployee(String customerId) async {
+//   try {
+//     // 1. ดึงข้อมูลของพนักงานจาก Firestore
+//     DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
+//         .collection('employees') // เปลี่ยนเป็น collection ที่คุณใช้จริง
+//         .doc(customerId)
+//         .get();
+
+//     if (!docSnapshot.exists) {
+//       throw Exception("ไม่พบข้อมูลพนักงานที่มี ID: $customerId");
+//     }
+
+//     // 2. ดึงค่าของ field 'imagePath' ที่เก็บ path ของรูป
+//     String filePath = docSnapshot.get('imagePath'); 
+
+//     if (filePath.isEmpty) {
+//       throw Exception("ไม่พบ path ของรูปภาพสำหรับพนักงาน ID: $customerId");
+//     }
+
+//     print("พบ imagePath: $filePath");
+
+//     // 3. ดึง URL ของรูปจาก Firebase Storage
+//     String downloadUrl =
+//         await FirebaseStorage.instance.ref(filePath).getDownloadURL();
+
+//     print("Download URL: $downloadUrl");
+
+//     return downloadUrl; // คืนค่า URL
+//   } catch (e, stacktrace) {
+//     print("เกิดข้อผิดพลาด: $e");
+//     print("รายละเอียดข้อผิดพลาด:\n$stacktrace");
+//     throw Exception("ไม่สามารถดึงรูปภาพได้");
+//   }
+// }
+Future<String> getImageEmpolyee(String filePath) async {
+    try {
+      String downloadUrl =
+          await FirebaseStorage.instance.ref(filePath).getDownloadURL();
+      print("Download URL: $downloadUrl");
+      return downloadUrl; // คืนค่า URL
+    } catch (e) {
+      print("เกิดข้อผิดพลาด: $e");
+      throw Exception(
+        "ไม่สามารถดึง URL ได้",
+      ); // ถ้าเกิดข้อผิดพลาด, โยน exception
+    }
+  }
+
+
+  //--------------------------------------------------------------------
+  // ข้อมูล customer
+  final CollectionReference customer = FirebaseFirestore.instance.collection(
+    'Customer',
+  );
+
+  Stream<String> getWhereUserIdStream(String email) {
+    final customerStream = customer
+        .where('userEmail', isEqualTo: email)
+        .snapshots()
+        .map((snapshot) {
+          if (snapshot.docs.isNotEmpty) {
+            final doc = snapshot.docs.first.data() as Map<String, dynamic>;
+            final userId = doc['userId'].toString();
+
+            return userId;
+          } else {
+            return 'ไม่พบข้อมูล';
+          }
+        });
+    return customerStream;
+  }
+
+  Stream<String> getuserNameStream(String userId) {
+    final customerStream = customer
+        .where('userId', isEqualTo: userId)
+        .snapshots()
+        .map((snapshot) {
+          if (snapshot.docs.isNotEmpty) {
+            final doc = snapshot.docs.first.data() as Map<String, dynamic>;
+            final userId = doc['userName'].toString();
+
+            return userId;
+          } else {
+            return 'ไม่พบข้อมูล';
+          }
+        });
+    return customerStream;
+  }
+
+  Future<bool> checkPhoneNoOnce(BuildContext context, String userId) async {
+    var querySnapshot = await customer.where('userId', isEqualTo: userId).get();
+
+    if (querySnapshot.docs.isEmpty) {
+      print("ไม่พบข้อมูลของชื่อนี้");
+      return false; // แก้ให้คืนค่า false ถ้าไม่พบข้อมูล
+    }
+
+    var userData = querySnapshot.docs.first.data() as Map<String, dynamic>;
+    if (userData.containsKey('phoneNum') &&
+        userData['phoneNum'].toString().isNotEmpty) {
+      print("มีเบอร์โทร: ${userData['phoneNum']}");
+      return true;
+    } else {
+      showPopup(
+        context,
+        "แจ้งเตือน",
+        "ไม่มีการบันทึกเบอร์โทร กรุณาเพิ่มเบอร์ที่ตั้งค่า",
+      );
+      return false;
+    }
+  }
+
+  Stream<String> getImageCustomerStream(String userId) {
+    return customer
+        .where('userId', isEqualTo: userId)
+        .limit(1) // จำกัดผลลัพธ์ให้ได้แค่ 1 เอกสาร
+        .snapshots() // รับข้อมูลแบบสตรีม
+        .map((snapshot) {
+          if (snapshot.docs.isNotEmpty) {
+            return snapshot.docs.first['userPhoto'] ?? 'ไม่พบข้อมูล';
+          }
+          return 'ไม่พบข้อมูล';
+        });
+  }
+
+  Stream<QuerySnapshot> getHistoryandBookmarkCustomerStream(String userId) {
+    final customerHistoryStream =
+        customer.where('userId', isEqualTo: userId).snapshots();
+    return customerHistoryStream;
+  }
+
+  // update
+  Future<void> addHistory(String userId, Map<String, dynamic> booking) async {
+    QuerySnapshot querySnapshot =
+        await customer.where('userId', isEqualTo: userId).get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      DocumentReference customerDoc = querySnapshot.docs.first.reference;
+
+      await customerDoc.update({
+        'bookingHistory': FieldValue.arrayUnion([booking]),
+      });
+
+      print("✅ เพิ่มข้อมูลการจองลงใน bookingHistory สำเร็จ");
+    } else {
+      print("❌ ไม่พบลูกค้าในฐานข้อมูล");
+    }
+  }
+
+  Future<void> addBookLocation(
+    String userId,
+    Map<String, dynamic> listMarkLocation,
+  ) async {
+    QuerySnapshot querySnapshot =
+        await customer.where('userId', isEqualTo: userId).get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      DocumentReference customerDoc = querySnapshot.docs.first.reference;
+
+      await customerDoc.update({
+        'addresses': FieldValue.arrayUnion([listMarkLocation]),
+      });
+
+      print("✅ เพิ่มข้อมูลการจองลงใน address สำเร็จ");
+    } else {
+      print("❌ ไม่พบลูกค้าในฐานข้อมูล");
+    }
+  }
+
+  Future<void> updateFullNameCustomer(
+    BuildContext context,
+    String userId,
+    String newUserName,
+  ) async {
+    if (newUserName.isEmpty) {
+      print('ไม่มีการเปลี่ยนชื่อ');
+    } else {
+      QuerySnapshot querySnapshot =
+          await customer.where('userId', isEqualTo: userId).get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        DocumentReference customerDoc = querySnapshot.docs.first.reference;
+
+        await customerDoc.update({
+          'userName': newUserName, // 🔄 เปลี่ยน userName เป็นค่าใหม่
+        });
+
+        print("✅ เปลี่ยนชื่อผู้ใช้เป็น '$newUserName' สำเร็จ!");
+      } else {
+        print("❌ ไม่พบผู้ใช้ที่มี userName = '$userId'");
+      }
+    }
+  }
+
+  Future<void> updatePhoneNumCustomer(String userId, String newPhoneNum) async {
+    if (newPhoneNum.isEmpty) {
+      print('ไม่มีการเปลี่ยนชื่อ');
+    } else {
+      QuerySnapshot querySnapshot =
+          await customer.where('userId', isEqualTo: userId).get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        DocumentReference customerDoc = querySnapshot.docs.first.reference;
+
+        await customerDoc.update({
+          'phoneNum': newPhoneNum, // 🔄 เปลี่ยน userName เป็นค่าใหม่
+        });
+
+        print("✅ เปลี่ยนเบอร์ใช้เป็น '$newPhoneNum' สำเร็จ!");
+      } else {
+        print("❌ ไม่พบผู้ใช้ที่มี userId = '$userId'");
+      }
+    }
+  }
+
+  Future<void> updatPasswordCustomer(String userId, String newPassword) async {
+    QuerySnapshot querySnapshot =
+        await customer.where('userId', isEqualTo: userId).get();
+
+    print("🟢 Firestore Query Completed!");
+
+    if (querySnapshot.docs.isNotEmpty) {
+      DocumentReference customerDoc = querySnapshot.docs.first.reference;
+      print("🟢 User found, updating password...");
+
+      // 🔄 ต้อง `await` update() เพื่อให้แน่ใจว่าเสร็จก่อนดำเนินการต่อ
+      await customerDoc.update({'userPassword': newPassword});
+
+      print("Password updated successfully for userId: $userId!");
+    } else {
+      print("No user found with userId: '$userId'");
+    }
+  }
+
+  //-------------------------
+  final CollectionReference booking = FirebaseFirestore.instance.collection(
+    'Booking',
+  );
+  // บันทึกการจอง
+  Future<void> addBooking(
+    String customer,
+    String empolyee,
+    String address,
+    String bookingDate,
+    String serviceDate,
+    String detail,
+    double totalPrice,
+    String status,
+  ) {
+    return booking.add({
+      'bookingId': DateTime.now().toString(),
+      'customer': customer,
+      'empolyee': empolyee,
+      'address': address,
+      'bookingDate': bookingDate,
+      'serviceDate': serviceDate,
+      'detail': detail,
+      'totalPrice': totalPrice,
+      'status': status,
+    });
+  }
+
+  ///-------------------image--------------------------------------------
+
+  
+}
